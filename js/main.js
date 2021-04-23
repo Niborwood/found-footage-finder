1a45f3bc26a52cf0112ef981edf41b8cc5a1b827
Niborwood <48519867+Niborwood@users.noreply.github.com>
2021-04-23 10:09:03 +0200

    v0.2 front ok without displayResult

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 // ************** CORE CODE *******************
-
-
 // PROTOTYPES
-let Flags = function (genre, difficulty, origin, date, theme) {
+const Flags = function (genre, difficulty, origin, date, theme) {
     this.genre = genre;
     this.difficulty = difficulty;
     this.origin = origin;
@@ -11,18 +9,19 @@ let Flags = function (genre, difficulty, origin, date, theme) {
 };
 
 // ----:::: GAME ::::----
-
-let game = {
+const game = {
     // --- GLITCH EFFECT
-    glitch: function () {
+    glitch: () => {
         let bgIOriginal = game.htmlElement.container.style.backgroundImage;
         game.htmlElement.container.style.backgroundImage = 'url(\'img/noise2.png\')';
         game.htmlElement.container.style.backgroundSize = 'initial';
+        game.htmlElement.container.style.animation = `animatedBackground ${Math.random()/2}s ease-in infinite`;
 
-        setTimeout(function () {
+        setTimeout(() => {
             game.htmlElement.container.style.backgroundImage = bgIOriginal;
+            game.htmlElement.container.style.animation = '0';
             game.htmlElement.container.style.backgroundSize = 'cover';
-        }, 100);
+        }, Math.random()*110);
     },
 
     // --- ELEMENTS HTML
@@ -32,6 +31,7 @@ let game = {
         mainButton: document.querySelector('main button'),
         launchButton: document.querySelector('button#launch-game'),
         playVhsString: document.querySelector('.play-vhs'),
+        appHeader: document.querySelector('h1'),
         appTitle: document.querySelector('#app-title'),
         container: document.querySelector('div#container')
     },
@@ -41,26 +41,51 @@ let game = {
         quizStep: 0,
         // Data des questions
         question: [
-            // Question 1
+            // Question 1 - Genre
             [
                 'Quel type de film souhaitez-vous voir ?',
-                'Un found footage',
-                'Un mockumentaire',
-                'Un film à sketchs',
-                'Une série épisodique',
+                ['Un found footage', 'ff'],
+                ['Un mockumentaire', 'mockumentary'],
+                ['Un film à sketchs', 'sketch'],
+                ['Une série épisodique', 'series'],
             ],
-            // Question 2
+            // Question 2 - Difficulty
             [
-                'Found footage de niche ou tranquille ?',
-                'Réponse 1',
-                'Réponse 2'],
-            // Question 3
-            'De quelle origine ?',
-            // Question 4
-            'Quelle époque ?',
-            // Question 5
-            'C\'est qui, vos tueurs préférés ?'
+                'Chaud pour un film de niche, ou on y va doucement ?',
+                ['Je connais déjà mes classiques, go !', 'hardcore'],
+                ['Sors-moi un truc regardable', 'casual']
+            ],
+            // Question 3 - origin
+            [
+                'Il doit venir d\'où, ce film ?',
+                ['de France', 'france'],
+                ['d\'Europe', 'europe'],
+                ['des USA', 'usa'],
+                ['d\'Asie', 'asia'],
+                ['Rien de tout ça', 'other']
+            ],
+            // Question 4 - date
+            [
+                'On part piocher à quelle époque ?',
+                ['les 70/80s', '70-80s'],
+                ['les 90s', '90s'],
+                ['les années 2000', '00s'],
+                ['de 2010 à aujourd\'hui', '10s']
+            ],
+            // Question 5 - theme
+            [
+                'Niveau horreur, votre kink, c\'est plutôt...',
+                ['Les slashers', 'slasher'],
+                ['Le paranormal', 'supernatural'],
+                ['Les aliens', 'aliens']
+            ],
             // Question 6
+            // [
+            //     'Si je rajoute une question...',
+            //     ['Oui', ''],
+            //     ['Non', '']
+            // ]
+            
             // Question 7
             // Question 8
             // Question 9
@@ -158,10 +183,9 @@ let game = {
     },
 
     // --- INITIALISATION DU JEU, POUR LA PREMIERE OU POUR UN REPLAY
-    init: function () {
-        if (game.htmlElement.playVhsString.innerText === 'STOP') {
-            game.htmlElement.playVhsString.innerText = '▶ PLAY';
-        }
+    init: () => {
+        game.htmlElement.playVhsString.innerText = '▶ PLAY';
+        game.htmlElement.playVhsString.classList.remove('animate-flicker');
         game.htmlElement.appTitle.remove();
         game.htmlElement.mainSection.innerText = '';
         game.htmlElement.mainButton.remove();
@@ -169,51 +193,118 @@ let game = {
         game.data.quizStep = 0;
     },
 
-    // ---- ROUTINE DE QUESTIONS
-    askQuestion: function () {
+    // ---- ROUTINE D'AFFICHAGE DE CHAQUE QUESTION/REPONSES
+    askQuestion: () => {
         game.glitch();
 
-        let currentQuestion = game.data.question[game.data.quizStep];
-        game.htmlElement.mainHeader.innerText = currentQuestion[0];
-
-        let answerButton = document.createElement('button');
+        // Elements HTML générés
+        const answerButton = document.createElement('button');
         answerButton.setAttribute('class', 'answer-button');
-        let answersWrapper = document.createElement('div');
+        const answersWrapper = document.createElement('div');
         answersWrapper.setAttribute('id', 'answer-wrapper');
         game.htmlElement.mainSection.appendChild(answersWrapper);
+        const nextStepStr = document.createElement('p');
+        nextStepStr.classList.add('next-question');
+        const skipQuestionString = 'Passer cette question (tout me va) <span class="forward">▶</span>';
+        const nextQuestionString = 'Question suivante <span class="forward">▶▶</span>';
+        nextStepStr.innerHTML = skipQuestionString;
+        game.htmlElement.mainSection.appendChild(nextStepStr);
+        
 
+        // Génération de l'affichage des questions / réponses (avec flag values)
+        const currentQuestion = game.data.question[game.data.quizStep];
+        game.htmlElement.mainHeader.innerText = currentQuestion[0];
         currentQuestion.shift();
         currentQuestion.forEach(answer => {
-            answerButton.innerText = answer;
+            answerButton.innerText = answer[0];
+            answerButton.value = answer[1];
             answersWrapper.appendChild(answerButton.cloneNode(true));
         });
 
-        let answerButtons = [...document.getElementsByClassName('answer-button')];
+        // Gestion de la réponse utilisateur et génération du bouton suivant
+        const answerButtons = [...document.getElementsByClassName('answer-button')];
+        
         answerButtons.forEach(button => {
             button.addEventListener('click', function (event) {
-                // Si n'importe quel bouton réponse est coché cliqué, on le déclique
-                // On rajoute l'état cliqué au bouton cliqué
-                let buttonClicked = document.getElementById('button-clicked');
+                const buttonClicked = document.getElementById('button-clicked');
+
                 if (buttonClicked === null) {
                     event.target.setAttribute('id', 'button-clicked');
-                    console.log('plop');
-                } else {
+                    
+                } else if (event.target.id === 'button-clicked') {
+                    buttonClicked.removeAttribute('id');
+                }
+                else {
                     buttonClicked.removeAttribute('id');
                     event.target.setAttribute('id', 'button-clicked');
                 }
+
+                if (nextStepStr.innerHTML === skipQuestionString && event.target.id === 'button-clicked') {
+                    nextStepStr.classList.add('animate-flicker', 'flicker__fast');
+                    nextStepStr.innerHTML = nextQuestionString;
+                } 
+                else if (nextStepStr.innerHTML === nextQuestionString && event.target.id !== 'button-clicked') {
+                    nextStepStr.innerHTML = skipQuestionString;
+                    nextStepStr.classList.remove('animate-flicker', 'flicker__fast');
+                }        
+            }); // Fin de l'EL au clic sur les boutons
+        }); // Fin de la boucle de boutons réponses
+        
+        // On enregistre le flag/réponse, on efface tout et on relance la prochaine question
+        nextStepStr.addEventListener('click', game.nextQuestion);
+    },
+
+    // ---- ROUTINE DE REPONSE A QUESTION / GENERATION DE NOUVELLE QUESTION / APPEL DE RESULTATS
+    nextQuestion: () => {
+        // On enregistre la (les) réponse(s) dans le tableau des flags/réponses
+        const userAnswer = document.getElementById('button-clicked');
+        if (userAnswer !== null) {     
+            game.data.answers.push(userAnswer.value);
+        } else {
+            const allAnswers = [...document.getElementsByClassName('answer-button')];
+            allAnswers.forEach(element => {
+                game.data.answers.push(element.value);
             });
-        });
+        }
+       
+        // Est-ce qu'on a fait le tour des questions à poser ?
+        if (game.data.quizStep < game.data.question.length-1) {
+            game.htmlElement.mainSection.innerText = '';
+            game.data.quizStep++;
+            game.askQuestion();
+        } else {
+            game.displayEndQuiz();
+        }
+    },
 
-        let nextStepStr = document.createElement('p');
-        nextStepStr.setAttribute('class', 'next-question');
-        nextStepStr.innerHTML = 'Question suivante <span class="forward">▶▶</span>';
-        game.htmlElement.mainSection.appendChild(nextStepStr);
+    // ---- ECRAN TEMPORAIRE DE FIN DE QUIZ
+    displayEndQuiz: () => {
+        game.glitch();
 
-        game.data.quizStep++;
+        // Gestion des éléments HTML
+        game.htmlElement.appHeader.appendChild(game.htmlElement.appTitle);
+        game.htmlElement.mainHeader.innerText = 'Votre found footage est prêt.';
+        game.htmlElement.mainSection.innerText = '';
+        const displayButton = document.createElement('button');
+        displayButton.innerHTML = '&#9679; Découvrez-le. &#9679;';
+        displayButton.classList.add('discover-button');
+        game.htmlElement.mainSection.appendChild(displayButton);
+        game.htmlElement.playVhsString.innerText = 'STOP';
+        game.htmlElement.playVhsString.classList.add('animate-flicker');
+
+        displayButton.addEventListener('click', game.displayResults);
+
+    },
+
+    // ---- AFFICHAGE DU RESULTAT
+    displayResults: () => {
+        console.log(game.data.answers);
+        
+
     },
 
     // --- EXECUTION GLOBALE DU JEU
-    play: function () {
+    play: () => {
 
         game.init();
         game.askQuestion();
@@ -246,3 +337,23 @@ function display_ct() {
     document.getElementById('current-time').innerHTML = x1;
     display_c();
 }
+
+game.data.answers = ['mockumentary', 'hardcore', 'europe', '90s', 'slasher'];
+
+// let movieFlagsData = game.data.movies[0].movie_data.flags;
+// let movieFlags = Object.values(movieFlagsData);
+
+// // console.log('Flags de la Data', movieFlags);
+// // console.log('Réponse utilisateur', game.data.answers);
+
+
+// // let matchingResults = [];
+
+
+// // ...check if element indexOf => movieFlags 
+// game.data.answers.forEach(element => {
+//     if (movieFlags.indexOf(element) === -1) {
+//         // moviesMatch.push(index);           
+//     } 
+// });
+
